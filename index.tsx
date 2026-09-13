e5f045569a3f4a3e6c696307a0575028b0cd1b59
Dian Buana Putra <71805094+DANDABP@users.noreply.github.com>
2026-09-14 03:01:39 +0700

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,158 @@
+import wrn45551 from "./4wrn4555-1.png";
+import group41 from "./group-41.png";
+import group42 from "./group-42.png";
+import group43 from "./group-43.png";
+import group49 from "./group-49.png";
+import group50 from "./group-50.png";
+import image from "./image.png";
+import line32 from "./line-32.svg";
+import line33 from "./line-33.svg";
+import rectangle35 from "./rectangle-35.png";
+import rectangle36 from "./rectangle-36.png";
+
+export const WeddingInvitation = (): JSX.Element => {
+  return (
+    <div className="bg-white w-full min-w-[700px] min-h-[5052px] relative">
+      <img
+        className="absolute w-[700px] h-[680px] top-[1562px] left-0"
+        alt="Group"
+        src={group50}
+      />
+      <img
+        className="absolute w-[700px] h-[680px] top-[2546px] left-0"
+        alt="Group"
+        src={group49}
+      />
+      <img
+        className="absolute top-0 left-0 w-[700px] h-[1050px] aspect-[0.67] object-cover"
+        alt="Element"
+        src={wrn45551}
+      />
+      <div className="absolute top-[511px] left-0 w-[700px] h-[541px] bg-[linear-gradient(180deg,rgba(14,49,84,0)_0%,rgba(14,49,84,0.5)_25%,rgba(14,49,84,0.75)_50%,rgba(14,49,84,1)_100%)]" />
+      <div className="absolute top-[3479px] left-0 w-[700px] h-[1573px] bg-[#0e3154]" />
+      <div className="absolute top-[641px] left-[207px] [font-family:'Elsie-Black',Helvetica] font-black text-white text-[23px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+        UNDANGAN PERNIKAHAN
+      </div>
+      <div className="absolute top-[816px] left-[302px] [font-family:'Elsie-Black',Helvetica] font-black text-white text-[28px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+        SABTU
+      </div>
+      <div className="absolute top-[667px] left-[132px] [font-family:'Elsie-Black',Helvetica] font-black text-white text-[70px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+        Mira &amp; Putra
+      </div>
+      <div className="absolute top-[2262px] left-[127px] [font-family:'Elsie-Black',Helvetica] font-black text-[#0e3154] text-[50px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+        Almira Alvinia D.p
+      </div>
+      <div className="absolute top-[3246px] left-[131px] [font-family:'Elsie-Black',Helvetica] font-black text-[#0e3154] text-[50px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+        Dian Buana Putra
+      </div>
+      <div className="absolute top-[3592px] left-[280px] [font-family:'Elsie-Black',Helvetica] font-black text-white text-[50px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+        Acara
+      </div>
+      <div className="top-[2384px] left-[315px] [font-family:'Elsie-Black',Helvetica] font-black text-[106px] whitespace-nowrap absolute text-[#0e3154] text-center tracking-[0] leading-[normal]">
+        &amp;
+      </div>
+      <p className="top-[2320px] left-[34px] absolute [font-family:'Elsie-Regular',Helvetica] font-normal text-[#0e3154] text-[21px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+        Putri Dari Bapak Dwi Piranggondani Santoso &amp; Ibu Nunuk Nurdianti
+      </p>
+      <p className="top-[3304px] left-[129px] absolute [font-family:'Elsie-Regular',Helvetica] font-normal text-[#0e3154] text-[21px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+        Putri Dari Bapak Pratikto &amp; Almh. Ibu Sunarwin
+      </p>
+      <p className="absolute top-[3660px] left-5 w-[660px] [font-family:'Elsie-Regular',Helvetica] font-normal text-white text-[21px] text-center tracking-[0] leading-[normal]">
+        Kami Bermaksud Untuk Mengundang Saudara/(i) Dalam Acara Pernikahan Kami
+        Pada:
+      </p>
+      <div className="absolute top-[848px] left-20 [font-family:'Elsie-Black',Helvetica] font-black text-white text-[59px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+        26 September 2026
+      </div>
+      <div className="absolute top-[778px] left-0 w-[700px] h-2 flex gap-2.5">
+        <img className="mt-[3px] w-[336px] h-0.5" alt="Line" src={line32} />
+        <div className="w-2 h-2 bg-white rounded aspect-[1]" />
+        <img className="mt-[3px] w-[336px] h-0.5" alt="Line" src={line33} />
+      </div>
+      <p className="absolute top-[1172px] left-[60px] w-[580px] [font-family:'Elsie-Regular',Helvetica] font-normal text-[#0e3154] text-[23px] text-center tracking-[0] leading-[normal]">
+        Dan Di Antara Tanda-tanda Kekuasaan-nya Ialah Dia Menciptakan Untukmu
+        Isteri-isteri Dari Jenismu Sendiri, Supaya Kamu Merasa Tenang Dan
+        Tentram Kepadanya, Dan Dijadikan-nya Diantaramu Rasa Kasih Dan Sayang.
+        Sesungguhnya Pada Yang Demikian Itu Benar-benar Terdapat Tanda-tanda
+        Bagi Kaum Yang Berfikir.
+        <br />
+        <br />
+        ar Rum: 21
+      </p>
+      <div className="top-[1480px] left-[60px] w-[580px] [font-family:'Elsie-Regular',Helvetica] font-normal text-[23px] [direction:rtl] absolute text-[#0e3154] text-center tracking-[0] leading-[normal]">
+        بسم الله الرحمن الرحيم
+      </div>
+      <p className="absolute top-[1516px] left-[60px] w-[580px] [font-family:'Elsie-Regular',Helvetica] font-normal text-[#0e3154] text-[23px] text-center tracking-[0] leading-[normal]">
+        Kami Mohon Do&#39;a &amp; Restunya Atas Pernikahan Kami
+      </p>
+      <img
+        className="absolute top-[1562px] left-[60px] w-[580px] h-[680px] object-cover"
+        alt="Rectangle"
+        src={rectangle35}
+      />
+      <img
+        className="absolute top-[2546px] left-[60px] w-[580px] h-[680px] object-cover"
+        alt="Rectangle"
+        src={rectangle36}
+      />
+      <img
+        className="absolute w-[700px] h-[194px] top-[954px] left-0"
+        alt="Group"
+        src={group41}
+      />
+      <img
+        className="absolute w-[700px] h-[194px] top-[3374px] left-0"
+        alt="Group"
+        src={group42}
+      />
+      <div className="absolute top-[3776px] left-[50px] w-[610px] h-[573px]">
+        <div className="absolute top-[35px] left-0 w-[600px] h-[538px] bg-white rounded-[9px]" />
+        <div className="absolute top-0 left-[150px] w-[300px] h-[70px] bg-white rounded-[9px] border-2 border-solid border-[#0e3154]" />
+        <div className="absolute top-[11px] left-[185px] [font-family:'Elsie-Black',Helvetica] font-black text-[#0e3154] text-[41px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+          Akad Nikah
+        </div>
+        <p className="absolute top-[90px] left-[152px] [font-family:'Elsie-Regular',Helvetica] font-normal text-[#0e3154] text-[28px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+          07.00 wib s/d 08.00 wib
+        </p>
+        <div className="absolute top-[223px] left-[102px] [font-family:'Elsie-Regular',Helvetica] font-normal text-[#0e3154] text-[28px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+          Balai Desa Kelurahan Burengan
+        </div>
+        <div className="absolute top-[126px] left-[98px] [font-family:'Elsie-Black',Helvetica] font-black text-[#0e3154] text-[32px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+          Sabtu, 26 September 2026
+        </div>
+        <p className="absolute top-[259px] left-[25px] w-[551px] [font-family:'Elsie-Black',Helvetica] font-black text-[#0e3154] text-[32px] text-center tracking-[0] leading-[normal]">
+          Jl. Letjend Sutoyo No.4, Bangsal, Kec. Pesantren, Kota Kediri
+        </p>
+        <img
+          className="absolute w-[150px] h-[150px] top-[363px] left-[225px] aspect-[1]"
+          alt="Group"
+          src={group43}
+        />
+      </div>
+      <div className="absolute top-[4379px] left-[50px] w-[610px] h-[573px]">
+        <div className="absolute top-[35px] left-0 w-[600px] h-[538px] bg-white rounded-[9px]" />
+        <div className="absolute top-0 left-[150px] w-[300px] h-[70px] bg-white rounded-[9px] border-2 border-solid border-[#0e3154]" />
+        <div className="absolute top-[11px] left-[221px] [font-family:'Elsie-Black',Helvetica] font-black text-[#0e3154] text-[41px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+          Resepsi
+        </div>
+        <p className="absolute top-[90px] left-[158px] [font-family:'Elsie-Regular',Helvetica] font-normal text-[#0e3154] text-[28px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+          11.00 wib s/d 13.00 wib
+        </p>
+        <div className="absolute top-[223px] left-[102px] [font-family:'Elsie-Regular',Helvetica] font-normal text-[#0e3154] text-[28px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+          Balai Desa Kelurahan Burengan
+        </div>
+        <div className="absolute top-[126px] left-[98px] [font-family:'Elsie-Black',Helvetica] font-black text-[#0e3154] text-[32px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+          Sabtu, 26 September 2026
+        </div>
+        <p className="absolute top-[259px] left-[25px] w-[551px] [font-family:'Elsie-Black',Helvetica] font-black text-[#0e3154] text-[32px] text-center tracking-[0] leading-[normal]">
+          Jl. Letjend Sutoyo No.4, Bangsal, Kec. Pesantren, Kota Kediri
+        </p>
+        <img
+          className="absolute w-[150px] h-[150px] top-[363px] left-[225px] aspect-[1]"
+          alt="Group"
+          src={image}
+        />
+      </div>
+    </div>
+  );
+};
